f502f8614c79ae5435c3d8e1b309bcbe74b89db9
Mohit Sudhakar <mohit.sudhakar@moneyview.in>
2019-04-17 10:03:04 +0530

    deleted unused files, rearranged packages

--- OCR/aadhar.py
+++ OCR/aadhar.py
@@ -8,43 +8,76 @@ import difflib
 import csv
 import nltk
 import subprocess
+import processor
+from operator import itemgetter
 from PIL import Image, ImageEnhance, ImageFilter
 
+
+def preprocess(imgpath, tR, tG, tB, processed_img):
+	img = Image.open(imgpath)
+	img = img.convert('RGBA')
+	# img = img.filter(ImageFilter.SHARPEN)
+	pix = img.load()
+
+	# Convert to black and white
+	for y in range(img.size[1]):
+		for x in range(img.size[0]):
+			if pix[x, y][0] < tR or pix[x, y][1] < tG or pix[x, y][2] < tB:
+				pix[x, y] = (0, 0, 0, 255)
+			else:
+				pix[x, y] = (255, 255, 255, 255)
+
+	img.save(processed_img)
+
+
 frontpath = sys.argv[1]
-# path = "/Users/mohit/Downloads/aadhar.jpg"
-
-img = Image.open(frontpath)
-img = img.convert('RGBA')
-# img = img.filter(ImageFilter.SHARPEN)
-pix = img.load()
-
-# Cropping
-for y in range(img.size[1]):
-    for x in range(img.size[0]):
-        if pix[x, y][0] < 102 or pix[x, y][1] < 102 or pix[x, y][2] < 102:
-            pix[x, y] = (0, 0, 0, 255)
-        else:
-            pix[x, y] = (255, 255, 255, 255)
-
-img.save('processed.jpg')
+frontpath_processed = 'frontpath_processed.jpg'
+rgb = 90
+preprocess(frontpath, rgb, rgb, rgb, frontpath_processed)
 
 # Call tesseract for orig image and sharpened image
-
 # original_text = pytesseract.image_to_string(img)
 # processed_text = pytesseract.image_to_string(Image.open('processed.jpg'))
 
-subprocess.call("tesseract "+frontpath+" original_text ", shell=True)
-# subprocess.call("tesseract processed.jpg processed_text ", shell=True)
-subprocess.call("tesseract processed.jpg processed_text  -l eng hin tsv", shell=True)
-# subprocess.call("tesseract processed.jpg processed_text_hin  -l hin csv", shell=True)
-#
-original_text_file = open('original_text.txt', 'r')
-original_text = original_text_file.read()
-processed_text_file = open('processed_text.txt', 'r')
+# subprocess.call("tesseract " + frontpath + " text -l eng tsv", shell=True)
+subprocess.call("tesseract " + frontpath_processed + " proctext -l eng tsv", shell=True)
+
+# original_text_file = open('text.tsv', 'r')
+# original_text = original_text_file.read()
+processed_text_file = open('proctext.tsv', 'r')
 processed_text = processed_text_file.read()
 
-original_text = filter(lambda x: ord(x) < 128, original_text)
+# original_text = filter(lambda x: ord(x) < 128, original_text)
 processed_text = filter(lambda x: ord(x) < 128, processed_text)
+
+print("------ Processed Text -----")
+
+parsed_word_list = processor.parse_tsv_with_confidence('proctext.tsv', 60)
+
+"""
+Need List of objects sorted in order of block, line, word
+"""
+
+block_map = {}
+
+parsed_word_list = sorted(parsed_word_list, key=itemgetter('block_num', 'line_num', 'word_num'))
+
+
+# print(parsed_word_list)
+for entry in parsed_word_list:
+	text = entry["text"]
+	conf = entry["conf"]
+	block = entry["block_num"]
+	par = entry["par_num"]
+	line = entry["line_num"]
+	word = entry["word_num"]
+	print(text, conf, block, par, line, word)
+
+
+
+
+
+"""
 
 # Initializing data variable
 name = None
@@ -157,31 +190,16 @@ pincode = None
 backpath = ''
 if len(sys.argv) > 2:
 	backpath = sys.argv[2]
+	backpath_processed = "processed_back.jpg"
 
-	img = Image.open(backpath)
-	img = img.convert('RGBA')
-	# img = img.filter(ImageFilter.SHARPEN)
-	pix = img.load()
+	preprocess(backpath, 102, 102, 102, backpath_processed)
 
-# cropping
-	for y in range(img.size[1]):
-		for x in range(img.size[0]):
-			if pix[x, y][0] < 102 or pix[x, y][1] < 102 or pix[x, y][2] < 102:
-				pix[x, y] = (0, 0, 0, 255)
-			else:
-				pix[x, y] = (255, 255, 255, 255)
+	subprocess.call("tesseract " + backpath + " text_back -l eng hin tsv ", shell=True)
+	subprocess.call("tesseract " + backpath_processed + " proctext_back -l eng hin tsv ", shell=True)
 
-	img.save('processed_back.jpg')
-
-	subprocess.call("tesseract "+backpath+" original_text_back ", shell=True)
-	# subprocess.call("tesseract processed_back.jpg processed_text_back ", shell=True)
-	subprocess.call("tesseract processed_back.jpg processed_text_back  -l eng hin tsv ", shell=True)
-	# subprocess.call("tesseract processed_back.jpg processed_text_back_hin  -l hin csv ", shell=True)
-
-
-	original_text_file = open('original_text_back.txt', 'r')
+	original_text_file = open('text_back.txt', 'r')
 	original_text = original_text_file.read()
-	processed_text_file = open('processed_text_back.txt', 'r')
+	processed_text_file = open('proctext_back.txt', 'r')
 	processed_text = processed_text_file.read()
 
 	original_text = filter(lambda x: ord(x) < 128, original_text)
@@ -220,4 +238,4 @@ with open('../result/' + os.path.basename(sys.argv[1]).split('.')[0] + '.json', 
 	json.dump(data, fp)
 
 
-
+"""
