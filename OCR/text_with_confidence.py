8e29661d4d8ae5a6bd526daab6b50b2a79eb610f
Mohit Sudhakar <mohit.sudhakar@moneyview.in>
2019-04-17 10:02:37 +0530

    extracted linewise text with given confidence

---
+++ OCR/text_with_confidence.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+import os.path
+import json
+import sys
+import pytesseract
+import re
+import difflib
+import csv
+import nltk
+import subprocess
+from utilities import processor
+from operator import itemgetter
+from collections import defaultdict
+from PIL import Image, ImageEnhance, ImageFilter
+
+
+def get(image, rgb, confidence_threshold):
+
+	# Variable Constants
+	processed_image = '/tmp/frontpath_processed.jpg'
+	processor.preprocess(image, rgb, processed_image)
+
+	# Call tesseract for orig image and sharpened image
+	# original_text = pytesseract.image_to_string(img)
+	# processed_text = pytesseract.image_to_string(Image.open('processed.jpg'))
+	# subprocess.call("tesseract " + frontpath + " text -l eng tsv", shell=True)
+	subprocess.call("tesseract " + processed_image + " proctext -l eng tsv", shell=True)
+	# original_text_file = open('text.tsv', 'r')
+	# original_text = original_text_file.read()
+	# processed_text_file = open('proctext.tsv', 'r')
+	# processed_text = processed_text_file.read()
+	# original_text = filter(lambda x: ord(x) < 128, original_text)
+	# processed_text = filter(lambda x: ord(x) < 128, processed_text)
+
+	print("------ Processed Text -----")
+
+	parsed_word_list = processor.parse_tsv_with_nonzero_confidence('proctext.tsv', confidence_threshold)
+	# print(parsed_word_list)
+	""" Need List of objects sorted in order of block, line, word """
+
+	block_map = {}
+
+	parsed_word_list = sorted(parsed_word_list, key=itemgetter('block_num', 'par_num', 'line_num', 'word_num'))
+
+	mylist = {}
+	mylist[0] = [parsed_word_list[0]["text"]]
+	j = 0
+	for i in range(1, len(parsed_word_list)):
+		if i == len(parsed_word_list):
+			break
+		entry = parsed_word_list[i]
+		previous_entry = parsed_word_list[i-1]
+		text = filter(lambda x: ord(x) > 31 and ord(x) < 128, entry["text"])
+		conf = entry["conf"]
+		block = entry["block_num"]
+		par = entry["par_num"]
+		line = entry["line_num"]
+		word = entry["word_num"]
+		print(text)
+		if block == previous_entry["block_num"] and par == previous_entry["par_num"] and line == previous_entry["line_num"]:
+			mylist[j].append(text)
+		else:
+			j = j + 1
+			mylist[j] = [text]
+
+	os.remove(processed_image)
+
+	print(mylist)
+	return mylist
+
+# TRY WITH 2 OR 3 DIFFERENT LIGHTING CONDITIONS (80, 100, 120)
+# get(sys.argv[1], 70, 30)
+# get(sys.argv[1], 80, 30)
+# get(sys.argv[1], 90, 30)
+# get(sys.argv[1], 100, 30)
+# get(sys.argv[1], 110, 30)
+get(sys.argv[1], 120, 30)
---
+++ OCR/utilities/processor.py
@@ -0,0 +1,49 @@
+import csv
+from PIL import Image
+
+
+def preprocess(imgpath, tRGB, processed_img):
+	tR = tRGB
+	tG = tRGB
+	tB = tRGB
+	img = Image.open(imgpath)
+	img = img.convert('RGBA')
+	# img = img.filter(ImageFilter.SHARPEN)
+	pix = img.load()
+
+	# Convert to black and white
+	for y in range(img.size[1]):
+		for x in range(img.size[0]):
+			if pix[x, y][0] < tR or pix[x, y][1] < tG or pix[x, y][2] < tB:
+				pix[x, y] = (0, 0, 0, 255)
+			else:
+				pix[x, y] = (255, 255, 255, 255)
+
+	img.save(processed_img)
+
+def parse_tsv_with_nonzero_confidence(file, confidence):
+    with open(file) as fd:
+        rd = csv.reader(fd, delimiter="\t", quotechar='"')
+        first_list = True
+        result_list = []
+        for row in rd:
+            if first_list:
+                first_list = False
+                continue
+            if int(row[10]) > 0 and int(row[10]) >= confidence and len(row[11].strip()) > 0:
+                entry = {
+                    # "level": row[0],
+                    # "page_num": row[1],
+                    "block_num": row[2],
+                    "par_num": row[3],
+                    "line_num": row[4],
+                    "word_num": row[5],
+                    # "left": row[6],
+                    # "top": row[7],
+                    # "width": row[8],
+                    # "height": row[9],
+                    "conf": row[10],
+                    "text": row[11]
+                }
+                result_list.append(entry)
+    return result_list
